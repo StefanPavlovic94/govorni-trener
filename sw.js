0b016c1b5889f119e9311c351b9d421f358a3ab5
Stefan Pavlovic <spstefan5@gmail.com>
2026-04-24 20:30:30 +0200

    Initial MVP

---
+++ sw.js
@@ -0,0 +1,24 @@
+﻿const CACHE = 'govorni-trener-v2';
+const ASSETS = ['./', './govorni-trener-mobile-preview.html', './manifest.webmanifest'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((r) => r || caches.match('./govorni-trener-mobile-preview.html')))
+  );
+});
